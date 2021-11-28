51b63b5b63bb807c07c7f32c20e162f2d6465f71
saddam-satria <ardhisaddam@gmail.com>
2021-11-28 19:17:20 +0700

    update: active class

--- app/Views/layouts/components/sidebar.php
+++ app/Views/layouts/components/sidebar.php
@@ -1,16 +1,17 @@
 <!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <!-- Sidebar - Brand -->
+  
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('dashboard'); ?>">
         <div class="sidebar-brand-text mx-3 text-capitalize"><i class="fas fa-hospital-symbol"></i> pharmacy</div>
     </a>
 
-    <!-- Divider -->
+
     <hr class="sidebar-divider my-0">
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
+
+
+    <li class="nav-item <?= uri_string() == "dashboard" ? "active" : "" ?>">
         <a class="nav-link" href="<?= base_url('dashboard'); ?>">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
@@ -24,25 +25,22 @@
     <div class="sidebar-heading">
         master data
     </div>
+    <li class="nav-item <?= uri_string() == "patients" ? "active" : "" ?>">
+        <a class="nav-link" href="<?= base_url('patients'); ?>">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Patients</span></a>
+    </li>
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="<?= base_url('patients') ?>" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-users-medical"></i>
-            <span class="text-capitalize">Patients</span>
-        </a>
+    <li class="nav-item <?= uri_string() == "users" ? "active" : "" ?>">
+        <a class="nav-link" href="<?= base_url('users'); ?>">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Users</span></a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="<?= base_url('users') ?>" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-user"></i>
-            <span class="text-capitalize">Users</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="<?= base_url('medicines') ?>" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas  fa-tablets"></i>
-            <span class="text-capitalize">Medicines</span>
-        </a>
+
+    <li class="nav-item <?= uri_string() == "medicines" ? "active" : "" ?>">
+        <a class="nav-link" href="<?= base_url('medicines'); ?>">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Medicines</span></a>
     </li>
 
     <!-- Divider -->
