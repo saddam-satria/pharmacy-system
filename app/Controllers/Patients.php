167e80a1aa6b14e40a311f00e473a0012d3c8e92
saddam-satria <ardhisaddam@gmail.com>
2021-11-30 00:47:45 +0700

    add: create patient

--- app/Controllers/Patients.php
+++ app/Controllers/Patients.php
@@ -4,10 +4,10 @@ namespace App\Controllers;
 
 use App\Controllers;
 
-
 class Patients extends BaseController
 {
     protected  $rules;
+    protected $db;
     public function __construct()
     {
         $this->rules = [
@@ -17,6 +17,7 @@ class Patients extends BaseController
             "last-visited" => ["rules" => "required", "errors" => ["required" => "please, fill last visited"]],
             "next-visited" => ["rules" => "required", "errors" => ["required" => "please, fill next visited"]]
         ];
+        $this->db = \Config\Database::connect();
     }
 
     public function addPatients()
@@ -24,9 +25,23 @@ class Patients extends BaseController
         if ($this->validate($this->rules)) {
             if ($this->request->getPost()) {
                 $patients = ["name" => $this->request->getVar('patients-name'), "address" => $this->request->getVar('patients-address'), "diseases" => $this->request->getVar('patients-diseases'), "last-visited" => $this->request->getVar('last-visited'), "next-visited" => $this->request->getVar('next-visited')];
-
                 // add to db
+                $result = $this->db->table('patients_data')->insert([
+                    "id_patients" => uniqid(),
+                    "name" => $patients['name'],
+                    "address" => $patients['address'],
+                    "diseases" => $patients['diseases'],
+                    "last_visited" => $patients["last-visited"],
+                    "next_visited" => $patients["next-visited"]
+                ]);
+
+                if (!$result) {
+                    return redirect()->back();
+                }
                 return view('add_patients', ["title" => "Add Patients", "validation" => $this->validator]);
+
+
+                // return view('add_patients', ["title" => "Add Patients", "validation" => $this->validator]);
             } else {
                 return redirect()->back();
             }
