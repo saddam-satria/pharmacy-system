9492d5954f90448cd27f43ac0c20ba1342147049
saddam-satria <ardhisaddam@gmail.com>
2021-11-30 01:01:19 +0700

    update: summary of patients

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -21,8 +21,8 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Patients (Monthly)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                Patients (Summary)</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $sumPatients; ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
