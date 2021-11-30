a0af67dcaa855413f52cbc65acddfd6b795da582
saddam-satria <ardhisaddam@gmail.com>
2021-11-30 15:50:32 +0700

    initialize patient model

---
+++ app/Models/PatientsModel.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PatientsModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'patients_data';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['id_patients', 'name', 'address', 'diseases', 'last_visited', 'next_visited'];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [
+        "id" => "required",
+        "id_patients" => "required|is_unique|max_length[100]",
+        "name" => "required|max_length[150]",
+        "address" => "required|max_length[200]",
+        "age"  => "required|numeric",
+        "diseases" => "required|max_length[100]",
+        "last_visited" => "required",
+        "next_visited" => "required"
+    ];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
